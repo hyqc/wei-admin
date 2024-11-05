242c6218945e42b84bd2c6a94e0afa4d98cd19f8
hyqc <1074966185@qq.com>
2024-11-05 22:59:50 +0800

    更新

--- admin-frontend/src/services/apis/admin/account.ts
+++ admin-frontend/src/services/apis/admin/account.ts
@@ -1,4 +1,3 @@
-import { PureSettings } from '@ant-design/pro-layout/lib/defaultSettings';
 import { request } from 'umi';
 import { ResponseBodyType } from '../types';
 import { APIAccount, APICommon } from './api';
@@ -8,53 +7,9 @@ export type MenusRemoteItem = {
   [key: string]: MenuDataItem;
 };
 
-// 当前登录返回的用户详情
-export type ReponseCurrentAdminUserinfoType = {
-  adminId: number;
-  username: string;
-  nickname: string;
-  avatar: string;
-  email: string;
-  token: string;
-  expire: number;
-  permissions: CurrentUserPermissionsType;
-  menus: MenusRemoteItem | null | undefined;
-  signature?: string;
-  settings?: PureSettings;
-  createTime?: string;
-  modifyTime?: string;
-  lastLoginTime?: string;
-  lastLoginIp?: string;
-  loginTotal?: number;
-  enabled?: boolean;
-};
 
-// 登录请求参数
-export type RequestLoginParamsType = {
-  username: string;
-  password: string;
-  remember?: boolean;
-};
 
-// 登录响应数据
-export type ResponseLoginDataType = ReponseCurrentAdminUserinfoType;
-
-export type RequestLogoutParamsType = {
-  id: number;
-};
-
-export type CurrentUserPermissionsType = {
-  [key: string]: string; // 权限名称:权限对应的接口路径
-};
-
-export type UploadImageFieldListItemType = {
-  uid: string;
-  name: string;
-  url: string;
-  status: string;
-};
-
-export async function login(params: RequestLoginParamsType) {
+export async function login(params: Req) {
   return request<ResponseBodyType>(APIAccount.login.url, {
     method: APIAccount.login.method,
     data: params,
